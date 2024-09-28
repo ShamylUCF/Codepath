93f464d05cafa7aa096a7a2c11650aca378bf0a4
ShamylUCF <sh923679@ucf.edu>
2024-09-28 13:28:58 -0400

    Add template code for Unit 2 Session 2 Questions 1-6

---
+++ Unit 2/Session 2/Question 1.py
@@ -0,0 +1,14 @@
+def find_balanced_subsequence(art_pieces):
+    pass
+
+
+
+
+
+art_pieces1 = [1,3,2,2,5,2,3,7]
+art_pieces2 = [1,2,3,4]
+art_pieces3 = [1,1,1,1]
+
+print(find_balanced_subsequence(art_pieces1))
+print(find_balanced_subsequence(art_pieces2))
+print(find_balanced_subsequence(art_pieces3))
