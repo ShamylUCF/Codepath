3446a2fce49a4dabd22edac3af6d65a294ba858d
ShamylUCF <sman7818@gmail.com>
2024-09-29 19:30:22 -0400

    Answering Session 1 Questions 4-6

--- Unit 2/Session 1/Question 6.py
+++ Unit 2/Session 1/Question 6.py
@@ -1,5 +1,20 @@
 def organize_pirate_crew(group_sizes):
-    pass
+    dic = {}
+    for pirate,group in enumerate(group_sizes):
+        if group in dic:
+            dic[group].append(pirate)
+        else:
+            dic[group] = []
+            dic[group].append(pirate)
+        
+    result = []
+
+    for key,value in dic.items():
+        for i in range(0, len(value), key):
+            result.append(value[i:i + key])
+
+    return result
+
 
 group_sizes1 = [3, 3, 3, 3, 3, 1, 3]
 group_sizes2 = [2, 1, 3, 3, 3, 2]
